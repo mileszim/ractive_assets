fec7f28079463657568cee247716f2586d536c7d
Cary FitzHugh <cary.fitzhugh@ziplist.com>
2013-10-22 15:48:57 -0400

    escape mustache contents

--- lib/ractive/rails/haml.rb
+++ lib/ractive/rails/haml.rb
@@ -8,11 +8,18 @@ module Ractive
 
       def prepare
         options = @options.merge(:filename => eval_file, :line => line)
-        @engine = ::Haml::Engine.new(data, options)
+        clean_shaven = data.gsub(/\{\{(.*)\}\}/) do |m|
+            "_MUSTACHIO_START_"+ Base64.strict_encode64(m)+"_MUSTACHIO_END_"
+          end
+        @engine = ::Haml::Engine.new(clean_shaven, options)
       end
 
       def evaluate(scope, locals, &block)
-        @engine.render(scope, locals, &block)
+        clean_shaven = @engine.render(scope, locals, &block)
+
+        data = clean_shaven.gsub(/_MUSTACHIO_START_(.*)_MUSTACHIO_END_/) do |m,a|
+          Base64.strict_decode64(m[17...-15])
+        end
       end
     end
   end
